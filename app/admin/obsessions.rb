f73f3baaaf4e1775ccdfdba4febb9460c9d54b1e
Raghu Betina <raghu@firstdraft.com>
2018-12-06 18:31:11 +0000

    Whitelist columns in Obsession for ActiveAdmin

--- app/admin/obsessions.rb
+++ app/admin/obsessions.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Obsession do
+
+ permit_params :stalker_id, :victim_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
